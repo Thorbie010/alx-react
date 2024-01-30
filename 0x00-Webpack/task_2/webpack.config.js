8fdc92dcc52d78dd19d8ffc616943390b8c3c02b
jonah <jonahemmanuel54@gmail.com>
2024-01-30 11:54:10 +0100

    task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './js/dashboard_main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
