090bd5f786c8ec34098565779cfe2b576af089ca
Jonah Emmanuel O <125548822+Thorbie010@users.noreply.github.com>
2024-04-08 18:42:11 +0100

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notification.js
+++
@@ -1,5 +0,0 @@
-import * as fileData from '../../../notifications.json'
-
-export function getAllNotificationsByUser(userId) {
-    return fileData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
-  }
